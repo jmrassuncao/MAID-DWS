1a25ebd72be4a2aaee8eb5bc2af23cae616c59af
Jorge Assunção <jorgeassuncao@users.noreply.github.com>
2017-10-18 16:30:01 +0100

    Changed LED controls to <Breathe.h>

--- code/MAID-DWS/MAID-DWS.cpp
+++ code/MAID-DWS/MAID-DWS.cpp
@@ -23,14 +23,31 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
+#include <Breathe.h>
 #include "../config/userdata_devel.h"
-
+//#include "../config/pin_mapping.h"
 
 //********************************************************************************************************
 const char* update_path = "/firmware";                                          // Path to firmware update page
 
-//the time when the sensor outputs a low impulse
-long unsigned int lowIn;
+//************* CONFIG PINS ******************************************************************************
+//********************************************************************************************************
+int sensorPin = D1;                                                             // Door sensor pin
+int r_ledPin = D6;                                                              // Red LED pin - door opened
+int g_ledPin = D7;                                                              // Green LED pin - door closed
+int b_ledPin = D8;                                                              // Blue LED pin - External running indicator
+int ledPin = D4;                                                                // Internal LED in NodeMCU
+
+//int ledState = LOW;             // ledState used to set the LED
+
+Breathe Breathe;    // Init breathe
+
+//********************************************************************************************************
+
+//unsigned long previousMillis = 0;        // will store last time LED was updated
+//const long interval = 1000;           // interval at which to blink (milliseconds)
+
+long unsigned int lowIn;    //the time when the sensor outputs a low impulse
 
 //the amount of milliseconds the sensor has to be low
 //before we assume all detection has stopped
@@ -40,8 +57,6 @@ long unsigned int pause = 100;
 boolean lockLow = true;
 boolean takeLowTime;
 
-int sensorPin = 13;                                                             //the digital pin connected to the door sensor's output
-
 //webserver
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
@@ -50,21 +65,26 @@ ESP8266HTTPUpdateServer httpUpdater;
 WiFiClient net;
 MQTTClient client;
 
-unsigned long lastMillis = 0;                                                   //Time Variable
+unsigned long lastMillis = 0;                                                   // Time Variable
 
 void connect();                                                                 // Connect to WiFI and MQTT
 
-
+//************* SETUP ************************************************************************************
+//********************************************************************************************************
 void setup()
 {
+
   pinMode(sensorPin, INPUT_PULLUP);
   digitalWrite(sensorPin, LOW);
+  pinMode(ledPin, OUTPUT);
+  pinMode(r_ledPin, OUTPUT);
+  pinMode(g_ledPin, OUTPUT);
+  pinMode(b_ledPin, OUTPUT);
 
   WiFi.config(ip, dns, gateway, subnet);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   client.begin(mqtt_server, net);
-  //client.onMessage(messageReceived);
 
   connect();
 
@@ -74,11 +94,14 @@ void setup()
   httpServer.begin();
 
   MDNS.addService("http", "tcp", 80);
+
+  //digitalWrite(r_ledPin, HIGH);   // TESTE
+  //digitalWrite(r_ledPin, LOW);
+
 }
 
 //Connect to wifi and MQTT
-void connect()
-{
+void connect() {
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
@@ -90,42 +113,44 @@ void connect()
   }
 }
 
+
+
+//************* LOOP *************************************************************************************
+//********************************************************************************************************
 void loop()
 {
-  // MQTT Loop
-  client.loop();
-  delay(10);
-
-  // Make sure device is connected
-  if(!client.connected())
-  {
-    connect();
-  }
 
   httpServer.handleClient();
 
-  if(digitalRead(sensorPin) == HIGH)                                            //Sensor Detection
-  {
-    if(lockLow)
+  Breathe.set(b_ledPin, HIGH, 1, 5 );    // https://github.com/kslstn/Breathe
+
+  if (!client.connected()) {                                                  // If client disconnects...
+    connect();                                                            // ...connect again
+  }
+
+  client.loop();
+
+    //
+  if (digitalRead(sensorPin) == HIGH) {                                            //Sensor Detection
+    if (lockLow)
     {
-      //makes sure we wait for a transition to LOW before any further output is made:
-      lockLow = false;
+      lockLow = false;    //makes sure we wait for a transition to LOW before any further output is made:
       client.publish(outTopic, "OPEN");
       delay(50);
     }
     takeLowTime = true;
   }
 
-  if(digitalRead(sensorPin) == LOW)
+  if (digitalRead(sensorPin) == LOW)
   {
-    if(takeLowTime)
+    if (takeLowTime)
     {
       lowIn = millis();                                                         //save the time of the transition from high to LOW
       takeLowTime = false;                                                      //make sure this is only done at the start of a LOW phase
     }
     //if the sensor is low for more than the given pause,
     //we assume that no more detection is going to happen
-    if(!lockLow && millis() - lowIn > pause)
+    if (!lockLow && millis() - lowIn > pause)
     {
       //makes sure this block of code is only executed again after
       //a new detection sequence has been detected
@@ -135,9 +160,11 @@ void loop()
     }
   }
 
+  // Debug.handle();                                                               // Remote debug over telnet
+
+  yield();                                                                      // Yielding
+
 }
 
-void messageReceived(String &topic, String &payload)
-{
-  //This sensor does not recieve anything from MQTT Server so this is blank
-}
+// END
+//********************************************************************************************************
