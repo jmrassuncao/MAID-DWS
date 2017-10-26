dff0e152f137c09480808d58e332dfa6e627974c
Jorge Assunção <jorgeassuncao@users.noreply.github.com>
2017-10-26 20:07:18 +0100

    Added missing variables

--- code/config/userdata.h
+++ code/config/userdata.h
@@ -10,20 +10,25 @@
 // and Hostname must be unique inside your network
 //********************************************************************************************************
 
+  #ifndef WIFI_CONFIG_H
 // Wifi SSID (the name of your wifi network)
-  const char* ssid = "wifi_ssid";
+  #define YOUR_WIFI_SSID "wifi_ssid";
 // Wifi password (the password to access your network)
-  const char* password = "wifi_password";
-// The fixed IP address you want for this node (must be unique inside your network - see above note)
-  IPAddress ip(192,168,1,222);
-// DNS server IP address (usually the same as your router/gateway but can be another DNS server)
-  IPAddress dns(8,8,8,8);
-// Gateway IP address (the address you use to access your router/gateway)
-  IPAddress gateway(192,168,1,1);
-// Subnet mask IP address ()
-  IPAddress subnet(255,255,255,0);
+  #define YOUR_WIFI_PASSWD "wifi_password";
+  #endif // !WIFI_CONFIG_H
+
+// If you want to use a fixed IP address, open "wifiConfig.h" and edit lines 123, 124 and 125
+
 // Hostname (the name you want to give to this node, must be unique inside your network)
-  #define host_name "your_hostname"
+  #define DEVICE_HOSTNAME "your_hostname"
+
+//************ CONFIG NTP ********************************************************************************
+// Configurations for the NTP server - Use the address of the server closer to your location and configure
+// your timezone relative to UTC
+//********************************************************************************************************
+  static const char ntpServerName[] = "ntp02.oal.ul.pt";
+
+  const int timeZone = 0;  // 0 = UTC
 
 //************ CONFIG ACCESS TO UPDATE PAGE **************************************************************
 // Configurations for OTA page - When you access "http://<device-ip>/firmware" the page will ask for a
@@ -31,49 +36,65 @@
 //********************************************************************************************************
 
 // Username to access the OTA page
-  const char* update_username = "admin";
+  const char* ACCESS_USERNAME = "admin";
 // Password to access the OTA page
-  const char* update_password = "1a2b3c";
+  const char* ACCESS_PASSWORD = "1a2b3c";
+// Path to update page
+  const char* UPDATE_PATH = "/firmware";
 
 //************* CONFIG MQTT ******************************************************************************
 // Configurations of your MQTT server -
 //********************************************************************************************************
 
 // MQTT server IP ou URL (the ip address or URL of your MQTT broker)
-  const char* mqtt_server = "mqtt_server_address";
-// MQTT port (usually 1883)
-  int mqtt_port = 1883;
+  const char* MQTT_SERVER = "mqtt_server_address";
 // MQTT user (the username of your MQTT broker)
-  const char* mqtt_username = "mqtt_user";
+  const char* MQTT_USERNAME = "mqtt_user";
 // MQTT password (the password of your MQTT broker)
-  const char* mqtt_password = "mqtt_password";
+  const char* MQTT_PASSWORD = "mqtt_password";
+// MQTT port (usually 1883)
+  int MQTT_PORT = 1883;
+
+//************ MQTT LWT **********************************************************************************
+// Configurations of your MQTT LWT - Define here the Last Will and Testment of your device. the MQTT broker and the
+// payload for door open and closed
+//********************************************************************************************************
+
+// MQTT last will topic
+  const char* MQTT_WILL_TOPIC = "home/indoor/sensor/MAID-DWS-01_t/status";
+// MQTT last will message
+  const char* MQTT_WILL_MESSAGE = "online";
+// MQTT last will QoS (0,1 or 2)
+  int MQTT_WILL_QOS = 1;
+// MQTT last will retain (0 or 1)
+  int MQTT_WILL_RETAIN = 1;
 
 //************ MQTT TOPICS *******************************************************************************
 // Configurations of your MQTT topics - to match your MQTT broker
 //********************************************************************************************************
 
 // MQTT topic to publish values
-  const char* outTopic = "home/indoor/sensor/MAID-DWS-01_t";
+  const char* MQTT_DOOR_STATE_TOPIC = "home/indoor/sensor/MAID-DWS-01_t";
 // MQTT payload for door open
-  const char* doorOpen = "Open";
+  const char* MQTT_DOOR_OPEN = "Open";
 // MQTT payload for door closed
-  const char* doorClosed = "Closed";
+  const char* MQTT_DOOR_CLOSED = "Closed";
 
 //************* CONFIG PINS ******************************************************************************
 // Configurations of the device pins - This is where you are going to connect the LEDs and sensor. You can
 // change them to fit your needs. The pins refer to what is marked on the board
 //********************************************************************************************************
 
-// Door sensor pin
-  int sensorPin = D1;
-// Red LED pin - door opened
-  int r_ledPin = D6;
-// Green LED pin - door closed
-  int g_ledPin = D7;
-// Blue LED pin - External running indicator
-  int b_ledPin = D8;
 // Internal LED in NodeMCU
-  int ledPin = D4;
+  #define ONBOARD_LED 2
+// Door sensor pin
+  #define SENSOR_PIN 5
+// Red LED pin - door opened
+  #define R_LED_PIN 12
+// Green LED pin - door closed
+  #define G_LED_PIN 13
+// Blue LED pin - External running indicator
+  #define B_LED_PIN 15
 
 // END
 //********************************************************************************************************
